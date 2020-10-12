d4b33066df5ba4aafa08197ac9430f7c3d28bc63
Owen <okornelsen@academic.rrc.ca>
2020-10-12 15:44:00 -0500

    Create function to handle loading CSV files to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+# PlayerPosition.delete_all
+# TeamFan.delete_all
+# Arena.delete_all
+# Country.delete_all
+# Fan.delete_all
+# Team.delete_all
+Position.delete_all
+# Player.delete_all
+
+def load_csv(csvfile)
+  filename = File.join(Rails.root, "db", csvfile)
+  puts "loading data from: #{filename}"
+  dataset = File.read(filename)
+  data = CSV.parse(dataset, headers: true)
+  puts "loaded #{data.size} rows from CSV file"
+  return data
+end
+
+positions = load_csv("position.csv")
+
+positions.each do |position|
+  puts position["position"]
+end
