d84d9b32cea3b0134f17a094058dc29c24896b69
Owen <okornelsen@academic.rrc.ca>
2020-10-13 17:44:07 -0500

    Generated Basic Controllers needed for Webpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/show'
+  get 'players/index'
+  get 'players/show'
+  get 'about/index'
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
