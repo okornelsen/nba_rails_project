90475195323bcece4cd34c97dd65d8f93de2e7a6
Owen <okornelsen@academic.rrc.ca>
2020-10-14 11:25:13 -0500

    Adjusted routes and populated basic index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'teams/index'
-  get 'teams/show'
-  get 'players/index'
-  get 'players/show'
-  get 'about/index'
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :players, only: %i[index show]
+  resources :teams, only: %i[index show]
+
+  root to: 'home#index'
 end
