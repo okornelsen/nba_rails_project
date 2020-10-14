ffa9e616bf8c7795e3ec0c277c966c84a4b2c258
Owen <okornelsen@academic.rrc.ca>
2020-10-14 14:23:23 -0500

    Addition of JQuery for Bootstrap and About Page

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap', '~> 5.0.0.alpha1'
+gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :players, only: %i[index show]
   resources :teams, only: %i[index show]
+  resources :about, only: %i[index]
 
-  root to: 'home#index'
+  root to: "home#index"
 end
