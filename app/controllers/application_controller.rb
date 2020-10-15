b92a7fe90562a8d8601ff6a6099f2ad98119c669
Owen <okornelsen@academic.rrc.ca>
2020-10-15 16:31:25 -0500

    Implemented Search function for all important data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def search
+    wildcard_search = "%#{params[:keywords]}%"
+    @players = Player.where("full_Name LIKE ?", wildcard_search)
+    @teams = Team.where("name LIKE ? OR city LIKE ?", wildcard_search, wildcard_search)
+    @countries = Country.where("name LIKE ?", wildcard_search)
+    @arenas = Arena.where("name LIKE ?", wildcard_search)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-
   resources :players, only: %i[index show]
   resources :teams, only: %i[index show]
   resources :about, only: %i[index]
   resources :countries, only: %i[index show]
   resources :arenas, only: %i[index show]
+  resources :home, only: %i[index] do
+    collection do
+      get "search"
+    end
+  end
 
   root to: "home#index"
 end
